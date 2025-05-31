b4211ab393daa42382275c47194f74a8fd69a826
Omar Mahboubi <omar_mahboubi@tutanota.com>
2025-05-31 14:23:24 +0100

    twrp_itel_P10003L.mk: remove reference to invalid file

--- twrp_itel_P10003L.mk
+++ twrp_itel_P10003L.mk
@@ -6,7 +6,6 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, device/android/common/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
